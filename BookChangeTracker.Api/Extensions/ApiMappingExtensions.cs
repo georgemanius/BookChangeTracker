0e7618c99194ecc9388f4df4387d80d03e6284ce
georgemanius <guravlev.g12@gmail.com>
2026-02-08 12:43:54 +0100

    Added mapping extensions for API and domain models

---
+++ BookChangeTracker.Api/Extensions/ApiMappingExtensions.cs
@@ -0,0 +1,59 @@
+using BookChangeTracker.Api.Models.Requests;
+using BookChangeTracker.Api.Models.Responses;
+using BookChangeTracker.Application.Models;
+
+namespace BookChangeTracker.Api.Extensions;
+
+public static class ApiMappingExtensions
+{
+    public static CreateBookDto ToCreateBookDto(this CreateBookRequest request) =>
+        new(
+            request.Title,
+            request.Description,
+            request.PublishDate);
+
+    public static UpdateBookDto ToUpdateBookDto(this UpdateBookRequest request) =>
+        new(
+            request.Title,
+            request.Description,
+            request.PublishDate);
+
+    public static CreateAuthorDto ToCreateAuthorDto(this CreateAuthorRequest request) => new(request.Name);
+
+    public static ChangeLogFilterDto ToChangeLogFilterDto(this ChangeLogFilterRequest request) =>
+        new(
+            request.FieldName,
+            request.FromDate,
+            request.ToDate,
+            request.ChangedBy,
+            request.OrderBy.ToString(),
+            request.SortOrder.ToString());
+
+    public static PaginationDto ToPaginationDto(this PaginationRequest request) =>
+        new(
+            request.PageNumber,
+            request.PageSize);
+
+    public static BookResponse ToResponse(this BookDto dto) =>
+        new(
+            dto.Id,
+            dto.Title,
+            dto.Description,
+            dto.PublishDate,
+            dto.Authors.Select(a => a.ToResponse()).ToList());
+
+    public static AuthorResponse ToResponse(this AuthorDto dto) => new(dto.Id, dto.Name);
+
+    public static BookChangeLogResponse ToResponse(this BookChangeLogDto dto) =>
+        new(
+            dto.Id,
+            dto.BookId,
+            dto.FieldName,
+            dto.OldValue,
+            dto.NewValue,
+            dto.ChangedAt,
+            dto.Description,
+            dto.ChangedBy,
+            dto.ChangeType,
+            dto.AuthorId);
+}
---
+++ BookChangeTracker.Application/Extensions/DomainMappingExtensions.cs
@@ -0,0 +1,56 @@
+using BookChangeTracker.Application.Models;
+using BookChangeTracker.Domain.Models.Entities;
+
+namespace BookChangeTracker.Application.Extensions;
+
+public static class DomainMappingExtensions
+{
+    public static Book ToBook(this CreateBookDto dto) =>
+        new()
+        {
+            Title = dto.Title,
+            Description = dto.Description,
+            PublishDate = dto.PublishDate
+        };
+
+    public static BookDto ToBookDto(this Book book) =>
+        new(
+            book.Id,
+            book.Title,
+            book.Description,
+            book.PublishDate,
+            book.BookAuthors.Select(ba => ba.Author.ToAuthorDto()).ToList());
+
+    public static void ApplyToBook(this UpdateBookDto dto, Book book)
+    {
+        if (dto.Title is not null)
+            book.Title = dto.Title;
+
+        if (dto.Description is not null)
+            book.Description = dto.Description;
+
+        if (dto.PublishDate.HasValue)
+            book.PublishDate = dto.PublishDate.Value;
+    }
+
+    public static Author ToAuthor(this CreateAuthorDto dto) =>
+        new()
+        {
+            Name = dto.Name
+        };
+
+    public static AuthorDto ToAuthorDto(this Author author) => new(author.Id, author.Name);
+
+    public static BookChangeLogDto ToBookChangeLogDto(this BookChangeLog log) =>
+        new(
+            log.Id,
+            log.BookId,
+            log.FieldName,
+            log.OldValue,
+            log.NewValue,
+            log.ChangedAt,
+            log.Description,
+            log.ChangedBy,
+            log.ChangeType.ToString(),
+            log.AuthorId);
+}
